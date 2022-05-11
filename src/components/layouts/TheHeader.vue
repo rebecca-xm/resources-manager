02f62f824ee00b2d5c81fa47fcc8989796f71945
Rebecca Mollica <rebecca.mollica@live.it>
2022-05-11 14:42:21 +0200

    feat: add header component

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+ <header>
+  <h1>{{ title }}</h1>
+ </header>
+</template>
+
+<script>
+export default {
+ props: ["title"],
+};
+</script>
+
+<style scoped>
+header {
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ background-color: #222f3e;
+ width: 100%;
+ height: 4rem;
+}
+
+header h1 {
+ color: #c8d6e5;
+ margin: 0;
+}
+</style>
