be5ffd5b70c72acc469e2548b45df3da27809d57
Rebecca Mollica <rebecca.mollica@live.it>
2022-05-12 09:57:19 +0200

    feat: add modale to warn user about invalid input

---
+++ src/components/UI/ResourceModale.vue
@@ -0,0 +1,76 @@
+<template>
+ <div>
+  <dialog open>
+   <header>
+    <slot name="header">
+     <h2>{{ title }}</h2>
+    </slot>
+   </header>
+   <section>
+    <slot></slot>
+   </section>
+   <menu>
+    <slot name="actions"></slot>
+   </menu>
+  </dialog>
+ </div>
+</template>
+
+<script>
+export default {
+ props: {
+  title: {
+   type: String,
+   required: false,
+  },
+ },
+};
+</script>
+
+<style scoped>
+div {
+ position: fixed;
+ top: 0;
+ left: 0;
+ height: 100vh;
+ width: 100%;
+ background-color: rgba(0, 0, 0, 0.75);
+ z-index: 1;
+}
+
+dialog {
+ position: fixed;
+ top: 20vh;
+ left: 10%;
+ width: 80%;
+ box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+ border-radius: 5px;
+ border: none;
+ padding: 0;
+ margin: 0;
+ overflow: hidden;
+ z-index: 2;
+}
+
+header {
+ background-color: #222f3e;
+ color: #c8d6e5;
+ width: 100%;
+ padding: 1rem;
+}
+
+header h2 {
+ margin: 0;
+}
+
+section {
+ padding: 1rem;
+}
+
+menu {
+ display: flex;
+ justify-content: right;
+ margin: 0;
+ padding: 1rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ResourceCard from './components/UI/ResourceCard.vue'
 import ResourceButton from "./components/UI/ResourceButton.vue";
+import ResourceModale from "./components/UI/ResourceModale.vue";
 
 
 createApp(App)
     .component('resource-card', ResourceCard)
     .component('resource-button', ResourceButton)
+    .component('resource-modale', ResourceModale)
     .mount('#app')
